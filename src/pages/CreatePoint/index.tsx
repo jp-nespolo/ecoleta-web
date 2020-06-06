a7e86f118b75bed57672e6cb3eef21751b0c2be8
Oseias Ayres <oseias_ayres@hotmail.com>
2020-06-05 20:33:35 -0400

    last updates from NLW

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -4,6 +4,8 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 
+import Dropzone from '../../components/Dropzone';
+
 import api from '../../services/api';
 import axios from 'axios';
 
@@ -35,19 +37,20 @@ const CreatePoint = () => {
     0,
   ]);
 
-  const [selectedItems, setSelectedItems] = useState<number[]>([]);
-  const [selectedUf, setSelectedUf] = useState('0');
-  const [selectedCity, setSelectedCity] = useState('0');
-  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
-    0,
-    0,
-  ]);
-
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     whatsapp: '',
   });
+
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -127,17 +130,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
 
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
     api.post('/points', data).then(() => {
       alert('Ponto criado');
       history.push('/');
@@ -160,7 +166,7 @@ const CreatePoint = () => {
           <br />
           ponto de coleta
         </h1>
-
+        <Dropzone onFileUploaded={setSelectedFile}></Dropzone>
         <fieldset>
           <legend>
             <h2>Dados</h2>
